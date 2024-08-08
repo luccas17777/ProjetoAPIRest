dc2b80f2fb8238e119ecf9770cc7ad6179336658
Luccas Eduardo Louzada de Morais <luccas.eduardo1@ibm.com>
2024-08-08 10:49:27 -0300

    implementando listar pessoas por nome, cep, e idade.

--- src/main/java/com/ibm/SerasaProject/repository/PessoaRepository.java
+++ src/main/java/com/ibm/SerasaProject/repository/PessoaRepository.java
@@ -4,6 +4,11 @@ import com.ibm.SerasaProject.model.pessoa.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+    List<Pessoa> findByNomeContainingIgnoreCase(String nome);
+    List<Pessoa> findByIdade(Integer idade);
+    List<Pessoa> findByCep(String cep);
 }
